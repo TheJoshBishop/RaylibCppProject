bf557010fdc140594ecf2411152174df5ead1909
joshua bishop2 <joshua.bishop2@student.centralia.edu>
2021-08-15 21:23:37 -0700

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ int main()
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    std::cout << "Test output" << std::endl;
     //--------------------------------------------------------------------------------------
 
     // Main game loop
